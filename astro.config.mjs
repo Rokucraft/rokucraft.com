7be45884eb788748dd00c3d981d0da368f12e01d
Aiko Vandeputte <aiko@aikovdp.me>
2023-04-04 16:38:49 +0200

    Add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   site: "https://new.rokucraft.com"
 });
