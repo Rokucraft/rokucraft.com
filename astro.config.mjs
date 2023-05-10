96f6e71d53689dbd6a20b1d429e6f27df909a99a
Aiko Vandeputte <aiko@aikovdp.me>
2023-05-10 15:43:47 +0200

    Update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://new.rokucraft.com",
+  site: "https://www.rokucraft.com",
   experimental: {
     assets: true
   }
