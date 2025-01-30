125c19ce5b86b420bae544f76a3e4a3f2ccc52a7
Aiko Vandeputte <aiko@we-are.be>
2025-01-30 21:40:55 +0100

    Add tailwind vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
   site: "https://www.rokucraft.com",
-})
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
